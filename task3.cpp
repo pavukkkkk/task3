359daa62e332f159c3e2cb9659777e072d81f75b
Konstantin <kirilenkokonstantin2003@gmail.com>
2021-10-07 00:28:53 +0400

    commit

---
+++ task3.cpp
@@ -0,0 +1,13 @@
+﻿// task3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+   int a;
+   cin >> a;
+   cout << a;
+   (a > 0) ? cout << ">0" : (a<0)? cout<<"<0" : cout<<"==0";
+  
+}
